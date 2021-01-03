11618832e45ff96c08a240d4be5dad7771806d14
Gwe2B <gwenael2b@live.fr>
2021-01-03 17:25:48 +0100

    run->switch->default state correction

--- src/com/Interpreteur.java
+++ src/com/Interpreteur.java
@@ -11,19 +11,14 @@ public class Interpreteur {
 /* ----------------------------- Class Constants ---------------------------- */
     public static final int MAX_MEM = 100;
 
-    //!UNUSED
-    //TODO: DELETE
-    /*public static final String[] MNE = {
-        "ADD", "SUB", "MUL", "DIV", "EQL", "NEQ", "GTR", "LSS", "GEQ", "LEQ",
-        "PRN", "INN", "INT", "LDI", "LDA", "LDV", "STO", "BRN", "BZE", "HTL"
-    };*/
-
 /* --------------------------- Instance attributes -------------------------- */
     private String[] PCODE;
     private int PC = 0;
 
     private int[] MEM = new int[Interpreteur.MAX_MEM];
     private int SP = 0;
+
+    private boolean PS = false;
 
 /* ------------------------------ Constructors ------------------------------ */
     /**
@@ -58,10 +53,31 @@ public class Interpreteur {
      * Interprete the P-CODE
      */
     public void run() {
-        while(this.PC < this.PCODE.length) {
+        this.PS = true;
+
+        while(this.PS && this.PC < this.PCODE.length) {
             String[] instruction = this.PCODE[this.PC++].split(" ");
             
             switch (instruction[0]) {
+                case "BZE":
+                    // while(condition) equivalent
+                    if(this.SP == 0)
+                        this.PC = Integer.parseInt(instruction[1]);
+                    else
+                        this.SP--;
+
+                    break;
+
+                case "BRN":
+                    // while(true) equivalent
+                    this.PC = Integer.parseInt(instruction[1]);
+                    break;
+
+                case "HLT":
+                    // stop the program
+                    this.PS = false;
+                    break;
+
                 case "INT":
                     //Réservation de X cases mémoires
                     this.SP += Integer.parseInt(instruction[1])-1;
@@ -91,8 +107,8 @@ public class Interpreteur {
                 case "STO":
                     /*Prend le sommet de pile le bouge de deux cases mémoires et
                       dépile deux fois*/
-                    this.SP -= 2;
-                    this.MEM[this.SP] = this.MEM[this.SP + 2];
+                    int buf = this.MEM[this.SP--];
+                    this.MEM[this.MEM[this.SP--]] = buf;
                     break;
 
                 case "PRN":
@@ -126,10 +142,34 @@ public class Interpreteur {
                 case "DIV":
                     this.MEM[--this.SP] = this.MEM[this.SP] / this.MEM[this.SP + 1];
                     break;
+
+                case "EQL":
+                    this.MEM[this.SP] = (this.MEM[this.SP] == this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
+
+                case "NEQ":
+                    this.MEM[this.SP] = (this.MEM[this.SP] != this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
+
+                case "GTR":
+                    this.MEM[this.SP] = (this.MEM[this.SP] > this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
+
+                case "LSS":
+                    this.MEM[this.SP] = (this.MEM[this.SP] < this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
+
+                case "GEQ":
+                    this.MEM[this.SP] = (this.MEM[this.SP] >= this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
+
+                case "LEQ":
+                    this.MEM[this.SP] = (this.MEM[this.SP] <= this.MEM[this.SP - 1]) ? 1 : 0;
+                    break;
             
                 default:
                     throw new RuntimeException("Unknown instruction: " +
-                        instruction);
+                        instruction[0] + " " + instruction[1]);
             }
         }
     }
